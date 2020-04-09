151012244935d986732eb9d126bca5d4a3e5ef9b
Adam J. Richardson <fatman.uk@gmail.com>
2020-04-09 10:50:51 +0100

    Implementing API again.

---
+++ src/plugins/mailbox.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "api.hh"
+
+using std::string;
+using std::cerr;
+using std::endl;
+
+const string PNAME = "mailbox";
+const string PVERSION = "v0.0.1";
+
+bool start(void *f) {
+	cerr << "Loading: " << PNAME << " " << PVERSION << "" << endl;
+#ifndef NDEBUG
+	cerr << PNAME << ": Start" << endl;
+#endif
+	auto fascia{reinterpret_cast<Fascia *>(f)};
+	// add ui bits to fascia f
+	// it will garbage collect
+	return true;
+}
+
+bool update() {
+#ifndef NDEBUG
+//	cerr << PNAME << ": Update" << endl;
+#endif
+	return true;
+}
+
+bool stop() {
+#ifndef NDEBUG
+	cerr << PNAME << ": Stop" << endl;
+#endif
+	return true;
+}
+
---
+++ src/plugins/rolodex.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "api.hh"
+
+using std::string;
+using std::cerr;
+using std::endl;
+
+const string PNAME = "rolodex";
+const string PVERSION = "v0.0.1";
+
+bool start(void *) {
+	cerr << "Loading: " << PNAME << " " << PVERSION << "" << endl;
+#ifndef NDEBUG
+	cerr << PNAME << ": Start" << endl;
+#endif
+	return true;
+}
+
+bool update() {
+#ifndef NDEBUG
+//	cerr << PNAME << ": Update" << endl;
+#endif
+	return true;
+}
+
+bool stop() {
+#ifndef NDEBUG
+	cerr << PNAME << ": Stop" << endl;
+#endif
+	return true;
+}
+
